c6906b8c4e06a9243136ecaad26bf48cea8daf6b
Georg Meißner <Georg.Meissner@mailbox.tu-dresden.de>
2020-08-21 10:11:40 +0200

    Initial Commit

---
+++ Shared/TemperaturTagebuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperaturTagebuchApp.swift
+//  Shared
+//
+//  Created by Georg Meissner on 21.08.20.
+//
+
+import SwiftUI
+
+@main
+struct TemperaturTagebuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
